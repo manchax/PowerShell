3e9ab94216fd0c1c1459153c74f0dc58df415855
Manuel Espinosa <manchax@gmail.com>
2015-03-29 11:09:27 -0700

    some enhancements

---
+++ Get-PublicIP.ps1
@@ -0,0 +1,8 @@
+﻿$url = "http://checkip.dyndns.com" 
+$webclient = New-Object System.Net.WebClient
+$ip = ($webclient.DownloadString($url)).Split(" ")[5].replace("</body>","").replace("</html>","")
+#print it
+$ip
+
+Remove-Item $env:USERPROFILE\MyIP.txt
+Remove-Variable ip, webclient, url
---
+++ Post-PublicIP.ps1
@@ -0,0 +1,14 @@
+﻿$url = "http://checkip.dyndns.com" 
+$webclient = New-Object System.Net.WebClient
+$ip = ($webclient.DownloadString($url)).Split(" ")[5].replace("</body>","").replace("</html>","")
+#print it
+$ip
+
+Remove-Item $env:USERPROFILE\MyIP.txt
+Remove-Variable ip, webclient, url
+
+#write IP to file
+$ip | Out-File $env:USERPROFILE\MyIP.txt
+
+#send it via pscp
+C:\pscp.exe -pw <PASSWORD> -scp $env:USERPROFILE\MyIP.txt <USER>@<HOST>: 
